e244d950005b359ec3493c5a33044c15d1080620
Oskar <oskaege@gmail.com>
2021-05-23 14:49:17 +1200

    GUI has search bar, drop down, and good resizing

--- src/main/java/weather/app/GUI.java
+++ src/main/java/weather/app/GUI.java
@@ -14,16 +14,65 @@ public class GUI {
         GridBagConstraints gbc = new GridBagConstraints();
 
         JLabel locationLabel = new JLabel("Location:");
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weighty = 0;
+        gbc.weightx = 0; //0 so the text field takes the free space
         gbc.gridx = 0;
         gbc.gridy = 0;
+        gbc.insets = new Insets(5, 5, 5, 5);
         mainFrame.add(locationLabel, gbc);
 
         JTextField inputField = new JTextField();
         inputField.setPreferredSize(new Dimension(200, 20));
+        gbc.fill = GridBagConstraints.HORIZONTAL; //don't want text field to be taller.
+        gbc.weighty = 0;
+        gbc.weightx = 0.5;
         gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.insets = new Insets(5, 5, 5, 5);
         mainFrame.add(inputField, gbc);
+
+        JButton searchButton = new JButton("Search");
+        searchButton.setPreferredSize(new Dimension(80, 20));
+        gbc.weighty = 0;
+        gbc.weightx = 0;
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        mainFrame.add(searchButton, gbc);
+
+        JLabel chooseLabel = new JLabel("Choose:");
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weighty = 0;
+        gbc.weightx = 0; //0 so the text field takes the free space
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        mainFrame.add(chooseLabel, gbc);
+
+        String[] testStrings = {"test 1", "test 2", "test 3"};
+        JComboBox<String> dropDown = new JComboBox<>(testStrings); 
+        inputField.setPreferredSize(new Dimension(200, 20));
+        gbc.fill = GridBagConstraints.HORIZONTAL; //don't want text field to be taller.
+        gbc.weighty = 0;
+        gbc.weightx = 0.5;
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        mainFrame.add(dropDown, gbc);
+
+        JButton goButton = new JButton("Go");
+        searchButton.setPreferredSize(new Dimension(80, 20));
+        gbc.weighty = 0;
+        gbc.weightx = 0;
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        mainFrame.add(goButton, gbc);
+
+        JLabel fillerTestLabel = new JLabel("This is a test to fill space");
+        gbc.weighty = 1; 
+        gbc.gridx = 0; 
+        gbc.gridy = 2; 
+        gbc.gridwidth = 3;
+        mainFrame.add(fillerTestLabel, gbc);
 
         mainFrame.pack();
     }
